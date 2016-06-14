d42b352f4b4bbbaec2427b69de26c6458271759d
lanjing <84336951@qq.com>
2016-06-14 17:44:34 +0800

    pod lib create LJTimerBlockManager

---
+++ LJTimerBlockManager/Example/LJTimerBlockManager/LJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LJAppDelegate.h
+//  LJTimerBlockManager
+//
+//  Created by lanjing on 06/14/2016.
+//  Copyright (c) 2016 lanjing. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
