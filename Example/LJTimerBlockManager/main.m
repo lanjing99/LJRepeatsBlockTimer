378aca22fd409d1258ce1da035abc90e4f4978a2
lanjing <84336951@qq.com>
2016-06-14 17:42:16 +0800

    Initial commit

---
+++ Example/LJTimerBlockManager/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LJTimerBlockManager
+//
+//  Created by lanjing on 06/14/2016.
+//  Copyright (c) 2016 lanjing. All rights reserved.
+//
+
+@import UIKit;
+#import "LJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LJAppDelegate class]));
+    }
+}
